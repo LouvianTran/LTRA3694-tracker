f9cce09748d2dea4ec1bfad8b7e6de2a33009c9b
LouvianTran <louviantran1@gmail.com>
2024-04-19 11:34:51 +1000

    Week 8 tut
    Installing parcel

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html')
+    // res.sendFile(__dirname + '/public/index.html') remove in week 8
+    res.sendFile(__dirname + '/dist/index.html')
    })
 
 let server = app.listen(8888, function(){
 console.log("App server is running on port 8888");
 });
+
+//Tutorial week 8
+app.use(express.static(__dirname + '/dist'));
